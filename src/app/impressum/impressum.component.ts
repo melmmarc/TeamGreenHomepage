6294822e5a0a8865f02e8f9c9fa1e91eca1fb28a
Marc Vogel <inf3462@hs-worms.de>
2023-03-15 11:39:00 +0100

    first commit

---
+++ src/app/impressum/impressum.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-impressum',
+  templateUrl: './impressum.component.html',
+  styleUrls: ['./impressum.component.scss']
+})
+export class ImpressumComponent {
+
+  constructor(private router: Router) {}
+
+}
