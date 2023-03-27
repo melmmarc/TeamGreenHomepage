5ef958fb8465d05064c6e6e3e4205b7519d65ae4
Marc Vogel <inf3462@hs-worms.de>
2023-03-27 20:16:59 +0200

    removed gallery

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,38 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
+  showPoster: boolean = false;
+  showScreenshots: boolean = false;
+  showVideo: boolean = false;
+
   constructor(private router: Router) {}
+
+  togglePoster(): void{
+    if(this.showPoster === false){
+      this.showPoster = true;
+    }
+    else{
+      this.showPoster = false;
+    }
+  }
+
+  toggleScreenshots(): void{
+    if(this.showScreenshots === false){
+      this.showScreenshots = true;
+    }
+    else{
+      this.showScreenshots = false;
+    }
+  }
+
+  toggleVideo(): void{
+    if(this.showVideo === false){
+      this.showVideo = true;
+    }
+    else{
+      this.showVideo = false;
+    }
+  }
+
 
 }
